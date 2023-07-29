788ffb9af83aafff4fd5e19faa0a6d9574edc8f0
Igor Kulkov <iakulkov@avito.ru>
2023-07-29 10:38:24 +0300

    fix

--- dto.go
+++ dto.go
@@ -254,10 +254,10 @@ func (in ReceiptIn) path() string        { return "/v1/receipt" }
 func (in ReceiptIn) out() (_ ReceiptOut) { return }
 
 type Brand struct {
-	Description string `json:"description"`
-	ID          int64  `json:"id"`
-	Image       string `json:"image"`
-	Name        string `json:"name"`
+	Description string  `json:"description"`
+	Id          int64   `json:"id"`
+	Image       *string `json:"image"`
+	Name        string  `json:"name"`
 }
 
 type Receipt struct {
